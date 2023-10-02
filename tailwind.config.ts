ce229951b071b8327b9d25a89a5b14d7a544e8b3
Balogun OLuwapelumi Johnson <pjohnsonPJB@gmail.com>
2023-10-02 15:28:55 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import colors from "tailwindcss/colors"
 
 const config: Config = {
   content: [
@@ -7,12 +8,53 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily :{
+      sans: ["Poppins", "Inter", "system-ui", "sans"],
+      monospace: ["DM Mono", "monospace"],
+    },
+    colors:{
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      green: colors.green,
+      orange: colors.orange,
+      purple: colors.purple,
+    pink: colors.pink,
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:{
+        bg: "#002244",
+        "fun-gray-light": "#b2bbcf",
+        "fun-gray": "#7b89a8",
+        "fun-gray-medium": "#767c85",
+        "fun-gray-darker": "#2a2a2c",
+        "fun-gray-dark": "#1F1F20",
+        "fun-gray-darkest": "#141414",
+        "fun-pink": "#00c7ff",
+        "fun-pink-darker": "#000f2e",
+        "fun-pink-darkest": "#000c24",
+        "fun-pink-dark": "#192742",
+        "fun-pink-light": "#009ac5",
       },
+      rotate: {
+        '360': '360deg'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'fadeInAndBounce': 'fadeIn 3s ease-out',
+      },
+      willChange: {
+        'projectCard': 'border-color, opacity, transform',
+      }
     },
   },
   plugins: [],
---
+++ utils/constants/routeLinks.ts
@@ -0,0 +1,28 @@
+type Route = {
+    title: string,
+    path: string
+  }
+  
+  
+  export const routes: Route[] = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    // {
+    //   title: "Blog",
+    //   path: "/blog",
+    // },
+    {
+      title: "Projects",
+      path: "/projects",
+    },
+    {
+      title: "Designs",
+      path: "/designs",
+    },
+  ];
+  
+  
+ 
+  
